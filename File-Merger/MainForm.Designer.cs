72895dd97cf702119431b726642efdcbd4ee696c
Discover- <amort11@hotmail.com>
2013-08-18 19:38:42 +0200

    Initial source of File-Merger.

---
+++ File-Merger/MainForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace File_Merger
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBoxExtensions = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxExtensions = new System.Windows.Forms.TextBox();
+            this.btnMerge = new System.Windows.Forms.Button();
+            this.txtBoxDirectory = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBoxIncludeSubDirs = new System.Windows.Forms.CheckBox();
+            this.checkBoxAllExtensions = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(145, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // checkBoxExtensions
+            // 
+            this.checkBoxExtensions.AutoSize = true;
+            this.checkBoxExtensions.Checked = true;
+            this.checkBoxExtensions.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxExtensions.Location = new System.Drawing.Point(17, 163);
+            this.checkBoxExtensions.Name = "checkBoxExtensions";
+            this.checkBoxExtensions.Size = new System.Drawing.Size(161, 17);
+            this.checkBoxExtensions.TabIndex = 1;
+            this.checkBoxExtensions.Text = "One output file per extension";
+            this.checkBoxExtensions.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(275, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Extensions to merge (split by semicolon if more than one):";
+            // 
+            // txtBoxExtensions
+            // 
+            this.txtBoxExtensions.Location = new System.Drawing.Point(13, 114);
+            this.txtBoxExtensions.Name = "txtBoxExtensions";
+            this.txtBoxExtensions.Size = new System.Drawing.Size(272, 20);
+            this.txtBoxExtensions.TabIndex = 3;
+            // 
+            // btnMerge
+            // 
+            this.btnMerge.Location = new System.Drawing.Point(209, 157);
+            this.btnMerge.Name = "btnMerge";
+            this.btnMerge.Size = new System.Drawing.Size(75, 23);
+            this.btnMerge.TabIndex = 4;
+            this.btnMerge.Text = "Merge!";
+            this.btnMerge.UseVisualStyleBackColor = true;
+            this.btnMerge.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtBoxDirectory
+            // 
+            this.txtBoxDirectory.Location = new System.Drawing.Point(13, 40);
+            this.txtBoxDirectory.Name = "txtBoxDirectory";
+            this.txtBoxDirectory.Size = new System.Drawing.Size(271, 20);
+            this.txtBoxDirectory.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Directory to search in:";
+            // 
+            // checkBoxIncludeSubDirs
+            // 
+            this.checkBoxIncludeSubDirs.AutoSize = true;
+            this.checkBoxIncludeSubDirs.Location = new System.Drawing.Point(17, 70);
+            this.checkBoxIncludeSubDirs.Name = "checkBoxIncludeSubDirs";
+            this.checkBoxIncludeSubDirs.Size = new System.Drawing.Size(129, 17);
+            this.checkBoxIncludeSubDirs.TabIndex = 7;
+            this.checkBoxIncludeSubDirs.Text = "Include subdirectories";
+            this.checkBoxIncludeSubDirs.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxAllExtensions
+            // 
+            this.checkBoxAllExtensions.AutoSize = true;
+            this.checkBoxAllExtensions.Location = new System.Drawing.Point(17, 140);
+            this.checkBoxAllExtensions.Name = "checkBoxAllExtensions";
+            this.checkBoxAllExtensions.Size = new System.Drawing.Size(90, 17);
+            this.checkBoxAllExtensions.TabIndex = 8;
+            this.checkBoxAllExtensions.Text = "All extensions";
+            this.checkBoxAllExtensions.UseVisualStyleBackColor = true;
+            this.checkBoxAllExtensions.CheckedChanged += new System.EventHandler(this.checkBoxAllExtensions_CheckedChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 188);
+            this.Controls.Add(this.checkBoxAllExtensions);
+            this.Controls.Add(this.checkBoxIncludeSubDirs);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBoxDirectory);
+            this.Controls.Add(this.btnMerge);
+            this.Controls.Add(this.txtBoxExtensions);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBoxExtensions);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "File-Merger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBoxExtensions;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxExtensions;
+        private System.Windows.Forms.Button btnMerge;
+        private System.Windows.Forms.TextBox txtBoxDirectory;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBoxIncludeSubDirs;
+        private System.Windows.Forms.CheckBox checkBoxAllExtensions;
+
+    }
+}
+
---
+++ File-Merger/MainForm.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace File_Merger
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            MinimizeBox = false;
+        }
+
+        private string allFiles;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string directory = txtBoxDirectory.Text;
+
+            if (directory == "")
+            {
+                MessageBox.Show("The directory field was left empty.", "An error has occurred!");
+                return;
+            }
+
+            if (!Directory.Exists(directory))// || !((File.GetAttributes(directory) & FileAttributes.Directory) == FileAttributes.Directory))
+            {
+                MessageBox.Show("The given directory does not exist.", "An error has occurred!");
+                return;
+            }
+
+            if (Path.HasExtension(directory))
+            {
+                MessageBox.Show("There is an extension in the directory.", "An error has occurred!");
+                return;
+            }
+
+            string extensionString = "";
+
+            if (!checkBoxAllExtensions.Checked)
+            {
+                extensionString = txtBoxExtensions.Text;
+
+                if (extensionString == "")
+                {
+                    MessageBox.Show("The extensions field was left empty.", "An error has occurred!");
+                    return;
+                }
+            }
+
+            //! Re-cursive call to get all files, then put them back in an array.
+            allFiles = ""; //! Reset this first so we don't get duplicates after pressing the Merge button more than once.
+            GetAllFilesFromDirectory(directory, checkBoxIncludeSubDirs.Checked);
+
+            if (allFiles == string.Empty)
+            {
+                MessageBox.Show("The searched directory contains no files at all.", "An error has occurred!");
+                return;
+            }
+
+            string[] arrayFiles = allFiles.Split('\n');
+
+            if (checkBoxAllExtensions.Checked)
+                for (int i = 0; i < arrayFiles.Length; i++)
+                    if (Path.HasExtension(arrayFiles[i]))
+                        extensionString += Path.GetExtension(arrayFiles[i]) + ";";
+
+            string[] extensionArray = extensionString.Split(';');
+            int z = checkBoxExtensions.Checked ? extensionArray.Length : 1;
+            bool firstLinePrinted = true;
+
+            for (int y = 0; y < z; ++y)
+            {
+                string extensionWithoutDot = extensionArray[y].Replace(".", "");
+                firstLinePrinted = false;
+
+                using (System.IO.StreamWriter outputFile = new System.IO.StreamWriter(directory + "\\merged_" + extensionWithoutDot + extensionArray[y], true))
+                {
+                    for (int i = 0; i < arrayFiles.Length; i++)
+                    {
+                        if (Path.HasExtension(arrayFiles[i]) && extensionArray[y].Contains(Path.GetExtension(arrayFiles[i])))
+                        {
+                            if (firstLinePrinted) //! First line has to be on-top of the file.
+                                outputFile.WriteLine("\t"); //! "\t" is a single linebreak, "\n" breaks two lines.
+
+                            firstLinePrinted = true;
+                            outputFile.WriteLine("-- - - - - - - - - - - - - - - - - - - - - - - - - - -");
+                            outputFile.WriteLine("-- '" + arrayFiles[i] + "'");
+                            outputFile.WriteLine("-- - - - - - - - - - - - - - - - - - - - - - - - - - -");
+                            outputFile.WriteLine("\t");
+
+                            string[] linesOfFile = System.IO.File.ReadAllLines(arrayFiles[i]);
+
+                            for (int j = 0; j < linesOfFile.Length; j++)
+                                outputFile.WriteLine("\t" + linesOfFile[j]);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void GetAllFilesFromDirectory(string directory, bool includingSubDirs)
+        {
+            string[] directories = Directory.GetDirectories(directory);
+            string[] files = Directory.GetFiles(directory);
+
+            for (int i = 0; i < files.Length; i++)
+                allFiles += files[i] + "\n";
+
+            //! If we include sub directories, recursive call this function up to every single directory.
+            if (includingSubDirs)
+                for (int i = 0; i < directories.Length; i++)
+                    GetAllFilesFromDirectory(directories[i], true);
+        }
+
+        private void checkBoxAllExtensions_CheckedChanged(object sender, EventArgs e)
+        {
+            //! No point in giving extensions if we will look for all extensions.
+            txtBoxExtensions.Enabled = !checkBoxAllExtensions.Checked;
+        }
+    }
+}
