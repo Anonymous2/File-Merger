c554594ad579a114b8449b725389528dcc9be725
Discover- <amort11@hotmail.com>
2013-08-20 12:23:53 +0200

    Add a separate textbox for the filename of the directory output so we have one for output file and output directory. Using the search button will fill in both fields to display the directory + filename (separated).

--- File-Merger/MainForm.Designer.cs
+++ File-Merger/MainForm.Designer.cs
@@ -38,12 +38,13 @@
             this.checkBoxIncludeSubDirs = new System.Windows.Forms.CheckBox();
             this.checkBoxAllExtensions = new System.Windows.Forms.CheckBox();
             this.btnSearchDirectory = new System.Windows.Forms.Button();
-            this.txtBoxDirectoryOutput = new System.Windows.Forms.TextBox();
+            this.txtBoxOutputDir = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.checkBoxSyncDirFields = new System.Windows.Forms.CheckBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.btnSearchForOutput = new System.Windows.Forms.Button();
             this.checkBoxDeleteOutputFile = new System.Windows.Forms.CheckBox();
+            this.txtBoxOutputFile = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // checkBoxExtensions
@@ -71,12 +72,12 @@
             // 
             this.txtBoxExtensions.Location = new System.Drawing.Point(14, 139);
             this.txtBoxExtensions.Name = "txtBoxExtensions";
-            this.txtBoxExtensions.Size = new System.Drawing.Size(272, 20);
+            this.txtBoxExtensions.Size = new System.Drawing.Size(315, 20);
             this.txtBoxExtensions.TabIndex = 3;
             // 
             // btnMerge
             // 
-            this.btnMerge.Location = new System.Drawing.Point(210, 205);
+            this.btnMerge.Location = new System.Drawing.Point(254, 205);
             this.btnMerge.Name = "btnMerge";
             this.btnMerge.Size = new System.Drawing.Size(75, 23);
             this.btnMerge.TabIndex = 4;
@@ -88,7 +89,7 @@
             // 
             this.txtBoxDirectory.Location = new System.Drawing.Point(14, 25);
             this.txtBoxDirectory.Name = "txtBoxDirectory";
-            this.txtBoxDirectory.Size = new System.Drawing.Size(247, 20);
+            this.txtBoxDirectory.Size = new System.Drawing.Size(294, 20);
             this.txtBoxDirectory.TabIndex = 5;
             // 
             // label3
@@ -123,7 +124,7 @@
             // 
             // btnSearchDirectory
             // 
-            this.btnSearchDirectory.Location = new System.Drawing.Point(261, 25);
+            this.btnSearchDirectory.Location = new System.Drawing.Point(305, 25);
             this.btnSearchDirectory.Name = "btnSearchDirectory";
             this.btnSearchDirectory.Size = new System.Drawing.Size(24, 20);
             this.btnSearchDirectory.TabIndex = 9;
@@ -131,12 +132,12 @@
             this.btnSearchDirectory.UseVisualStyleBackColor = true;
             this.btnSearchDirectory.Click += new System.EventHandler(this.btnSearchDirectory_Click);
             // 
-            // txtBoxDirectoryOutput
+            // txtBoxOutputDir
             // 
-            this.txtBoxDirectoryOutput.Location = new System.Drawing.Point(14, 95);
-            this.txtBoxDirectoryOutput.Name = "txtBoxDirectoryOutput";
-            this.txtBoxDirectoryOutput.Size = new System.Drawing.Size(247, 20);
-            this.txtBoxDirectoryOutput.TabIndex = 5;
+            this.txtBoxOutputDir.Location = new System.Drawing.Point(14, 95);
+            this.txtBoxOutputDir.Name = "txtBoxOutputDir";
+            this.txtBoxOutputDir.Size = new System.Drawing.Size(220, 20);
+            this.txtBoxOutputDir.TabIndex = 5;
             // 
             // label1
             // 
@@ -166,7 +167,7 @@
             // 
             // btnSearchForOutput
             // 
-            this.btnSearchForOutput.Location = new System.Drawing.Point(261, 95);
+            this.btnSearchForOutput.Location = new System.Drawing.Point(305, 94);
             this.btnSearchForOutput.Name = "btnSearchForOutput";
             this.btnSearchForOutput.Size = new System.Drawing.Size(24, 20);
             this.btnSearchForOutput.TabIndex = 11;
@@ -187,20 +188,28 @@
             this.checkBoxDeleteOutputFile.Text = "Delete output file if exists";
             this.checkBoxDeleteOutputFile.UseVisualStyleBackColor = true;
             // 
+            // txtBoxOutputFile
+            // 
+            this.txtBoxOutputFile.Location = new System.Drawing.Point(235, 95);
+            this.txtBoxOutputFile.Name = "txtBoxOutputFile";
+            this.txtBoxOutputFile.Size = new System.Drawing.Size(73, 20);
+            this.txtBoxOutputFile.TabIndex = 13;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(300, 234);
-            this.Controls.Add(this.checkBoxDeleteOutputFile);
+            this.ClientSize = new System.Drawing.Size(341, 234);
             this.Controls.Add(this.btnSearchForOutput);
+            this.Controls.Add(this.txtBoxOutputFile);
+            this.Controls.Add(this.checkBoxDeleteOutputFile);
             this.Controls.Add(this.checkBoxSyncDirFields);
             this.Controls.Add(this.btnSearchDirectory);
             this.Controls.Add(this.checkBoxAllExtensions);
             this.Controls.Add(this.checkBoxIncludeSubDirs);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtBoxDirectoryOutput);
+            this.Controls.Add(this.txtBoxOutputDir);
             this.Controls.Add(this.txtBoxDirectory);
             this.Controls.Add(this.btnMerge);
             this.Controls.Add(this.txtBoxExtensions);
@@ -229,11 +238,12 @@
         private System.Windows.Forms.CheckBox checkBoxIncludeSubDirs;
         private System.Windows.Forms.CheckBox checkBoxAllExtensions;
         private System.Windows.Forms.Button btnSearchDirectory;
-        private System.Windows.Forms.TextBox txtBoxDirectoryOutput;
+        private System.Windows.Forms.TextBox txtBoxOutputDir;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.CheckBox checkBoxSyncDirFields;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button btnSearchForOutput;
         private System.Windows.Forms.CheckBox checkBoxDeleteOutputFile;
+        private System.Windows.Forms.TextBox txtBoxOutputFile;
     }
 }
